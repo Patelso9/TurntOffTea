276e5647ff3546f1f8884da42272dccc7f470603
Princess <sonali.patel.pkg@gmail.com>
2023-12-11 16:48:18 -0600

    update link & img

--- src/Landing/SocialLanding.jsx
+++ src/Landing/SocialLanding.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material'
 import React from 'react'
 import './SocialLanding.css'
-import logo from '../img/tot_logo2.png'
+import logo from '../img/tot_banner.png'
 
 
 
@@ -11,10 +11,8 @@ const SocialLanding = () => {
         
      {/* <h1 className='social-landing-title'>Turnt off Tea</h1> */}
 
-     <img src={logo} alt="Turnt off Tea logo" style={{width: "40%"}}/>
+     <img src={logo} alt="Turnt off Tea logo" style={{width: "100%", marginTop: "-55px"}}/>
      <br />
-
-     <h3>Let's get turnt!</h3>
 
     {/* <Button
          variant="outlined" 
@@ -24,7 +22,6 @@ const SocialLanding = () => {
     <br /> <br /> */}
  
   
-
     <Button
         href="https://www.amazon.com/Productivity-Optimized-Business-Professionals-Academic/dp/B07RBS1QFH/ref=sr_1_1_sspa?crid=2S7HKV4ULQ49V&keywords=roterunner%2Bpurpose%2Bplanner&qid=1701295152&sprefix=purpose%2Bplanner%2Brote%2Caps%2C100&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1" 
         variant="outlined" 
@@ -34,32 +31,49 @@ const SocialLanding = () => {
         <h3>:  Purpose Planner by Roterunner</h3> </Button> 
     <br /> <br />
 
+    <Button
+        href="https://amzn.to/3tmk5YZ" 
+        variant="outlined" 
+        size="large" 
+        style = {{width:'80%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
+        >Aotesier
+        <h3>: Liquid Silicone iPhone Case</h3> </Button> 
+    <br /> <br />
+
+    <Button
+        href="https://eilieangels.goaffpro.com/shop/EA10AM50?category=xmx4lhqb" 
+        variant="outlined" 
+        size="large" 
+        style = {{width:'80%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
+        >Eilie Angels
+        <h3>: Collection of elegant everyday & dressy jewlery </h3> </Button> 
+    <br /> <br />
+
+
+
 
 
     <Button
         href="https://www.pinterest.com/turntofftea/" 
         variant="outlined" 
         size="large" 
-        style = {{width:'80%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
+        style = {{width:'20%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
         >Pinterest</Button> 
-    <br /> <br />
 
    <Button
         href="https://instagram.com/turntofftea?igshid=OGQ5ZDc2ODk2ZA==" 
         variant="outlined" 
         size="large" 
-        style = {{width:'80%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
+        style = {{width:'20%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
         >Instagram</Button> 
-    <br /> <br />
 
     <Button
         href="https://github.com/Patelso9"  
         variant="outlined" 
         size="large" 
-        style = {{width:'80%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
+        style = {{width:'20%' , textTransform:'none', color:"black", borderColor:"white", backgroundColor:"rgba(255,255,255,0.5)", fontFamily: "Didot, serif" }}
         
         >GitHub</Button> 
-    <br />
 
 
     </div>
